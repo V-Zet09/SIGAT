7e44d258aa6106ca4a604772bc25c7a031d6e0c2
Zaet Maico <zaet_maico@hotmail.com>
2025-10-08 19:04:39 -0600

    cambios master y sidebar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,20 +18,16 @@
     <meta content="SIGAT" name="author" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('images/LOGO_VENTANA_SF.png') }}">
-
-    <!-- Librerías externas -->
     <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          integrity="sha512-..."
           crossorigin="anonymous"
           referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <!-- RemixIcon -->
-    <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
-
-    {{-- Vite: Estilos y JS principales --}}
     @vite([
         'resources/scss/bootstrap.scss',    
         'resources/scss/icons.scss',
@@ -40,27 +36,20 @@
         'resources/js/app.js'
     ])
 
-    {{-- CSS adicional por vista --}}
     @yield('css')
 </head>
 
-<body>
-    <!-- Begin page -->
+<body class="bg-gray-50 text-gray-900 transition-colors duration-200">
     <div id="layout-wrapper">
         
-        {{-- Sidebar opcional --}}
         @hasSection('sidebar')
             @yield('sidebar')
         @else
             @include('layouts.sidebar')
         @endif
 
-        <!-- ============================================================== -->
-        <!-- Start main content -->
-        <!-- ============================================================== -->
-        <div class="main-content" style="margin-left: 250px;">
+        <div class="main-content lg:ml-64 bg-gray-50 transition-colors duration-200">
             
-            {{-- Navbar opcional --}}
             @hasSection('navbar')
                 @yield('navbar')
             @endif
@@ -71,21 +60,14 @@
                 </div>
             </div>
 
-            {{-- Footer --}}
             @include('layouts.footer')
 
         </div>
-        <!-- end main content-->
     </div>
-    <!-- END layout-wrapper -->
 
-    {{-- Vendor scripts --}}
     @include('layouts.vendor-scripts')
-
-    {{-- Scripts por vista --}}
     @yield('scripts')
 
-    <!-- Flowbite -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.5.1/flowbite.min.js"></script>
 </body>
 </html>
